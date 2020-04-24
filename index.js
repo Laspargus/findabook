b7e2845450451312b512773fd9c3c2a345425d0d
laspargus <maxime.speroni@gmail.com>
2020-04-24 11:34:04 +0200

    add recommendation

--- index.js
+++ index.js
@@ -1,68 +1,285 @@
-//const apikey = window.prompt("Insert your OpenMovieApiKey");
+let selection = [];
+let recommendations = [];
+console.log(selection);
+let alertMessage = document.querySelector("#alert-message");
 
-////FUNCTION TO GET VIDEO TRAILER
-const getMovieDbUrl = (imdbId) => {
-  let url = `https://api.themoviedb.org/3/movie/${imdbId}/videos?api_key=c5d88044659a5ab6d5965a2c9ef028e0&language=en-US`;
+////FUNCTION TO GET LIST OF BOOKS
+const searchBook = (searchValue) => {
+  let url = `https://www.googleapis.com/books/v1/volumes?q=${searchValue}`;
+  getBook(url);
+};
+
+const getDbBook = (url) => {
   fetch(url)
     .then((response) => response.json())
-    .then((response) => findYouTubeId(response.results))
-    .catch((error) => console.error("error:", error));
+    .then((response) => dislayTop100(response))
+    .catch((error) => console.log("error:", error));
 };
 
-const findYouTubeId = (data) => {
-  console.log(data[0].key);
-  let youtubeId = data[0].key;
-  document.querySelector("#video").innerHTML = `
-  <iframe
-    width="400"
-    height="300"
-    src="https://www.youtube.com/embed/${youtubeId}"
-    frameborder="0"
-    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-    allowfullscreen
-  ></iframe>`;
-};
-
-////FUNCTION TO GET LIST OF MOVIES
-const searchMovie = (searchValue) => {
-  let url = `https://www.omdbapi.com/?apikey=b9017d50&s=${searchValue}`;
-  getMovies(url);
-};
-
-const getMovies = (url) => {
+const getBook = (url) => {
   fetch(url)
     .then((response) => response.json())
-    .then((response) => displayPreview(response.Search))
+    .then((response) => displayPreview(response.items))
     .catch((error) => console.error("error:", error));
 };
 
-////// FUNCTION TO GET SPECIFIC MOVIE
-const searchSpecificMovie = (imdbId) => {
-  let urlOne = `https://www.omdbapi.com/?apikey=b9017d50&i=${imdbId}`;
-  getSpecificMovie(urlOne);
+getDbBook("http://localhost:3000/books");
+
+const selectBook = (title, author, image, isbn13, isbn10, year) => {
+  alertMessage.innerHTML = "";
+  alertMessage.classList.add("not-visible");
+
+  let pair = {};
+  pair["title"] = title;
+  pair["author"] = author;
+  pair["image"] = image;
+  pair["isbn13"] = isbn13;
+  pair["isbn10"] = isbn10;
+  pair["year"] = year;
+  let validation = true;
+  console.log(selection);
+
+  selection.forEach((item) => {
+    if (item.title == title) {
+      console.error("you already selected this book");
+      alertMessage.classList.remove("not-visible");
+      alertMessage.innerHTML = "you already selected this book";
+      validation = false;
+    }
+  });
+
+  if (selection.length == 5) {
+    console.error("you already have 5 books");
+    alertMessage.classList.remove("not-visible");
+    alertMessage.innerHTML =
+      "you already selected 5 books. Validate your choice.";
+    validation = false;
+  }
+
+  if (validation) {
+    selection.push(pair);
+  }
+  document.querySelector("#selection").classList.remove("not-visible");
+  document.querySelector("#selection").innerHTML = "";
+  displaySelection();
 };
 
-const getSpecificMovie = (urlOne) => {
-  fetch(urlOne)
+const displaySelection = () => {
+  document.querySelector("#howtoremove").classList.remove("not-visible");
+  for (let i = 0; i < selection.length; i++) {
+    if (i == 5) {
+      break;
+    }
+    let img = selection[i].image;
+    document.querySelector("#selection").innerHTML += `
+  <div class="text-center col-md-2">
+  <img class="" src="${img}" onclick="deleteBook('${img}')" style="width:100px">
+  </div>
+    `;
+  }
+  if (selection.length === 5) {
+    displayButton();
+  }
+};
+
+const deleteBook = (img) => {
+  document.querySelector("#validate-selection").classList.add("not-visible");
+  for (let i = 0; i < selection.length; i++) {
+    if (selection[i].image == img) {
+      selection.splice(i, 1);
+    }
+  }
+  document.querySelector("#selection").innerHTML = "";
+  if (selection.length >= 1) {
+    displaySelection();
+  }
+  console.log(selection);
+};
+
+const displayButton = () => {
+  document.querySelector("#validate-selection").classList.remove("not-visible");
+  document.querySelector("#validate-selection").innerHTML = `
+  <button
+  type="submit"
+  id="validate"
+  class="btn btn-warning"
+  onclick="saveSelection()">Confirm Selection</button>`;
+};
+
+const saveSelection = () => {
+  getSelections();
+  document.querySelector("#loader").classList.remove("not-visible");
+
+  fetch("http://localhost:3000/selections", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(selection),
+  })
     .then((response) => response.json())
-    .then((response) => {
-      displayModale(response);
-    })
-    .catch((error) => console.error("error:", error));
+    .then((response) => console.log(response))
+    .catch((error) => console.log("error:", error));
 };
 
 ///// FUNCTION TO DISPLAY LIST OF MOVIES
 const displayPreview = (data) => {
-  document.querySelector("#movieList").innerHTML = "";
+  document.querySelector("#bookList").innerHTML = "";
 
   for (let i = 0; i < data.length; i++) {
-    let date = data[i].Year;
-    let title = data[i].Title;
-    let image = data[i].Poster;
-    let imdbId = data[i].imdbID;
+    console.log(data[i]);
+    let year = "";
+    let image = "/bookcover.png";
+    let title = "";
+    let author = "";
+    let isbn10 = "unknown";
+    let isbn13 = "unknown";
 
-    document.querySelector("#movieList").innerHTML += `
-    <div class="not-visible previewMovie card mb-3">
+    if ("authors" in data[i].volumeInfo) {
+      author = data[i].volumeInfo.authors[0];
+    }
+
+    if (data[i].volumeInfo.industryIdentifiers[0].type == "OTHER") {
+      continue;
+    } else {
+      isbn13 = data[i].volumeInfo.industryIdentifiers[0].identifier;
+      isbn10 = data[i].volumeInfo.industryIdentifiers[0].identifier;
+    }
+
+    if ("title" in data[i].volumeInfo) {
+      title = data[i].volumeInfo.title.replace(/'/g, "\\'");
+    }
+
+    if ("imageLinks" in data[i].volumeInfo) {
+      image = data[i].volumeInfo.imageLinks.smallThumbnail;
+    } else {
+      continue;
+    }
+
+    if ("publishedDate" in data[i].volumeInfo) {
+      let year = data[i].volumeInfo.publishedDate;
+    }
+
+    document.querySelector("#bookList").innerHTML += `
+    <div id="book${isbn13}" onclick="selectEffect(${isbn13})">
+    <img class="m-3" onclick="selectBook('${title}', '${author}','${image}','${isbn13}','${isbn10}', '${year}')" src="${image}" style="width:150px">
+    </div>
+    `;
+  }
+};
+
+document.querySelector("#searchForm").addEventListener("submit", (event) => {
+  event.preventDefault();
+  const searchValue = document.querySelector("input[type='text']").value;
+  searchBook(searchValue);
+});
+
+const selectEffect = (id, event) => {
+  console.log(event);
+  console.log(id);
+  anime({
+    targets: `#book${id}`,
+    translateY: -10000,
+    //borderRadius: 50,
+    duration: 2000,
+    easing: "linear",
+    // direction: "alternate",
+  });
+};
+
+dislayTop100 = (response) => {
+  response.forEach((book) => {
+    let image = book.image;
+    let title = book.title.replace(/'/g, "\\'");
+    let author = book.author.replace(/'/g, "\\'");
+    let year = book.year;
+    let isbn13 = book.ISBN_13;
+    let isbn10 = book.ISBN_10;
+
+    document.querySelector("#top100").innerHTML += `
+  <div id=book${isbn13} onclick="selectEffect(${isbn13}, event)" class="booktop100">
+    <img class="m-3" id="bookImage" src="${image}" onclick="selectBook('${title}', '${author}','${image}','${isbn13}','${isbn10}', '${year}')"  style="width:150px">
+  </div>
+  `;
+  });
+};
+
+const getSelections = () => {
+  fetch("http://localhost:3000/selections")
+    .then((response) => response.json())
+    .then((response) => findTwinSelections(response))
+    .catch((error) => console.error("error:", error));
+};
+
+const findTwinSelections = (allDbSelections) => {
+  allDbSelections.forEach((dbSelection) => {
+    dbSelection.books.forEach((book) => {
+      for (let i = 0; i < selection.length; i++) {
+        if (book.ISBN_13 == selection[i].isbn13) {
+          let newSelection = dbSelection.books.filter(
+            (book) => book.ISBN_13 != selection[i].isbn13
+          );
+          recommendations.push(newSelection);
+        }
+      }
+    });
+  });
+  getFinalArray();
+};
+
+const getFinalArray = () => {
+  vrac = [];
+  recommendations.forEach((grap) => {
+    grap.forEach((book) => vrac.push(book));
+  });
+
+  var finalarray = [];
+  var copy = vrac.slice(0);
+  for (var i = 0; i < vrac.length; i++) {
+    var myCount = 0;
+    // loop over every element in the copy and see if it's the same
+    for (var w = 0; w < copy.length; w++) {
+      if (copy[w] == undefined) {
+        continue;
+      } else {
+        if (vrac[i].ISBN_13 == copy[w].ISBN_13) {
+          myCount++;
+          delete copy[w];
+        }
+      }
+    }
+    if (myCount > 0) {
+      var a = new Object();
+      a.value = vrac[i];
+      a.count = myCount;
+      finalarray.push(a);
+    }
+  }
+  let sortedFinalArray = finalarray.sort((a, b) =>
+    a.count < b.count ? 1 : -1
+  );
+  console.log(sortedFinalArray);
+  displayRecommendation(sortedFinalArray);
+};
+
+const displayRecommendation = (sortedFinalArray) => {
+  document.querySelector("#recommendation").innerHTML += `
+  <h2 class="mb-5 recommendation-title text-info">
+        Here are your 5 next must-read books :
+      </h2>
+  `;
+  for (let i = 0; i < 5; i++) {
+    if (i > sortedFinalArray.length) break;
+
+    let image = sortedFinalArray[i].value.image;
+    let title = sortedFinalArray[i].value.title;
+    let date = sortedFinalArray[i].value.year;
+    let isbn13 = sortedFinalArray[i].value.ISBN_13;
+    let author = sortedFinalArray[i].value.author;
+
+    document.querySelector("#recommendation").innerHTML += `
+    <div class="previewMovie card mb-3">
     <div class="row no-gutters">
       <div class="col-md-3">
       <img style="width:150px" src="${image}" class="card-img" alt="${title}" />
@@ -73,90 +290,15 @@ const displayPreview = (data) => {
           ${title}
           </p>
           <p class="card-text">
-            <small class="text-muted">${date}</small>
+            <small class="text-muted">${author}</small>
           </p>  
         </div>
       </div>
       <div class="pt-5 col-md-2">
-        <button id="${imdbId}" onclick="searchSpecificMovie(this.id)" class="btn btn-warning mb-2">Read More</button>
+        <button id="${isbn13}" onclick="" class="btn btn-warning mb-2">Read More</button>
       </div>
     </div>
   </div>
     `;
   }
-  let observer = new IntersectionObserver(
-    (observables) => {
-      observables.forEach((observable) => {
-        if (observable.intersectionRatio > 0.9) {
-          observable.target.classList.remove("not-visible");
-        }
-      });
-    },
-    { threshold: [0.9] }
-  );
-
-  let movies = document.querySelectorAll(".previewMovie");
-  movies.forEach((movie) => observer.observe(movie));
 };
-
-///////// FUNCTION TO GET MODAL
-
-const displayModale = (data) => {
-  console.log(data);
-  let date = data.Year;
-  let title = data.Title;
-  let image = data.Poster;
-  let description = data.Plot;
-  let imdbId = data.imdbID;
-
-  console.log(imdbId);
-
-  document.getElementById("movieContent").innerHTML = `
-  <div class="mb-3">
-    <div class="row no-gutters">
-      <div class="col-md-5">
-      <img style="width:350px" src="${image}" class="card-img" alt="${title}" />
-      </div>
-      <div class="col-md-7">
-        <div class="card-body">
-          <h3 class="card-text">
-          ${title}
-          </h3>
-          <p class="card-text">
-          ${description}
-          </p>
-          <p class="card-text">
-            <small class="text-muted">${date}</small>
-          </p>
-          <p id="video" class="card-text">
-          </p>
-        </div>
-      </div>
-    </div>
-  </div>
-  `;
-
-  let myModal = document.getElementById("myModal");
-  let video = document.querySelector("#video");
-  myModal.style.display = "block";
-
-  let span = document.getElementsByClassName("close")[0];
-  span.onclick = function () {
-    myModal.style.display = "none";
-    video.style.display = "none";
-  };
-
-  window.onclick = function (event) {
-    if (event.target == myModal) {
-      myModal.style.display = "none";
-      video.style.display = "none";
-    }
-  };
-  getMovieDbUrl(imdbId);
-};
-
-document.querySelector("#searchForm").addEventListener("submit", (event) => {
-  event.preventDefault();
-  const searchValue = document.querySelector("input[type='text']").value;
-  searchMovie(searchValue);
-});
